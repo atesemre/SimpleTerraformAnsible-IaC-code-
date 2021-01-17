b6eba1f8bd248d4041c31ebb913cad0397ca0f83
Shubham Girdhar <shubhamgirdhar94@gmail.com>
2021-01-18 00:43:44 +0100

    Adding terraform and ansible configuration files and steps for the project

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+location = "eastus"
+resource_group = "NoBSDevOpsMonolith"
+cloud_shell_source = ""
+domain_name_prefix = "nobs"
+management_ip = ""
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+    type = string
+}
+
+variable "resource_group" {
+    type = string
+}
+
+variable "cloud_shell_source" {
+    type = string
+}
+
+variable "management_ip" {
+    type = string
+}
+
+variable "domain_name_prefix" {
+    type = string
+}
