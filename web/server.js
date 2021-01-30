42332502ed8c45b2d132fc1b4a8579d1e0c2e500
Shubham Girdhar <shubhamgirdhar94@gmail.com>
2021-01-30 13:29:38 +0100

    deleting unnecessary folders

--- web/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./src/app");
-
-app.listen(3000, () => {
-  console.log("running on port 3000");
-  console.log("--------------------------");
-});
--- web/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
-});
-
-module.exports = app;
